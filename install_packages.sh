a361c7d34fd676bf2909fa69efeac15fff52a60e
Rcoppee <66408977+Rcoppee@users.noreply.github.com>
2025-02-20 08:16:41 +0100

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 read -p "In order to use this automated analysis pipeline, several (python and R) software packages are required. The purpose of this script is to facilitate the use of the pipeline by automatically installing packages/software. Would you like to proceed with this installation? (Y/n) " choix_installation
@@ -7,13 +6,16 @@ if [ "$choix_installation" != "Y" ]; then
     echo "You have chosen not to install packages."
     exit 1
 else
+    sudo apt update
+    sudo apt install libcurl4-openssl-dev libxml2-dev libssl-dev libfontconfig1-dev libharfbuzz-dev libfribidi-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev
+    sudo apt install g++-11
+    
     # Vérification et installation de rate4site
     if echo "0" | rate4site -h &> /dev/null; then
         echo "rate4site is already installed."
     else
         echo "rate4site is not installed."
         echo "Installing..."
-        sudo apt update
         sudo apt install rate4site
         echo "rate4site is installed."
     fi
@@ -24,7 +26,6 @@ else
     else
         echo "tkinter is not installed."
         echo "Installation in progress..."
-        sudo apt update
         sudo apt install python3-tk
         echo "tkinter is installed."
     fi
@@ -34,7 +35,7 @@ else
     else
         echo "customtkinter is not installed."
         echo "Installation in progress..."
-        pip3 install customtkinter
+        sudo pip3 install customtkinter --break-system-packages
         echo "customtkinter is installed."
     fi
 
@@ -71,7 +72,13 @@ else
     else
         echo "msa is not installed."
         echo "Installation in progress..."
-        sudo Rscript -e 'BiocManager::install("msa")'
+        wget https://www.bioconductor.org/packages/release/bioc/src/contrib/msa_1.38.0.tar.gz
+        sudo Rscript -e 'BiocManager::install("Biostrings")'
+        sudo Rscript -e 'BiocManager::install("BiocGenerics")'
+        sudo Rscript -e 'BiocManager::install("IRanges")'
+        sudo Rscript -e 'BiocManager::install("S4Vectors")'
+        sudo R CMD INSTALL msa_1.38.0.tar.gz --configure-args="CXXFLAGS=-std=c++11"
+        #sudo Rscript -e 'BiocManager::install("msa")'
         echo "msa is installed."
     fi
 fi
